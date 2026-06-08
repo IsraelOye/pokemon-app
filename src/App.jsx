6c69d85911cee2df84a956f00fda71e46671e1e7
Israel Oyedele <israeloyedele533@gmail.com>
2026-06-08 11:47:37 +0100

    [IO][sm] changed padding on app.jsx --feat

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import AppRoutes from "./routes/AppRoutes";
 
 function Home() {
   return (
-    <div className="px-8 py-5 bg-black min-h-screen">
+    <div className="px-4 md:px-8 py-5 bg-black min-h-screen">
       <AppRoutes />
     </div>
-  )
+  );
 }
 
 export default Home
